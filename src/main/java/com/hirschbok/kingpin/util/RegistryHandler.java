9a7ab60796433c9ca486fe9ccee764ba793fda15
Charlie Shifrin <21cshifrin@gunston.org>
2021-02-26 20:50:38 -0500

    initial commit

---
+++ src/main/java/com/hirschbok/kingpin/util/RegistryHandler.java
@@ -0,0 +1,19 @@
+package com.hirschbok.kingpin.util;
+
+import com.hirschbok.kingpin.items.syringe;
+import com.hirschbok.kingpin.kingpin;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+    public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, kingpin.MOD_ID);
+
+    public static void init(){
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+    public static final RegistryObject<Item> SYRINGE = ITEMS.register("syringe", syringe::new);
+}
+
