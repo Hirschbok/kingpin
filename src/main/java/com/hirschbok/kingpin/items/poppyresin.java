8724d5c7081bfceb87e46e546c7d14b9670dec84
Hirschbock <52053770+Hirschbok@users.noreply.github.com>
2021-02-28 16:08:39 -0500

    Created poppy resin file

---
+++ src/main/java/com/hirschbok/kingpin/items/poppyresin.java
@@ -0,0 +1,11 @@
+package com.hirschbok.kingpin.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class poppyresin extends Item {
+
+    public poppyresin() {
+        super(new Item.Properties().group(ItemGroup.FOOD));
+    }
+}
